3bfa0b2080975b3752cdd1f39f6e6fda432a44d8
Syed <syedahmed.aviation@gmail.com>
2024-10-04 12:50:22 +0400

    Add page loading state to website with PageLoading component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 import AboutSection from "./Components/AboutSection/AboutSection";
 import CodePG from "./Components/CodePG/CodePG";
@@ -9,21 +11,38 @@ import MyNextBigMovesSection from "./Components/MyNextBigMovesSection/MyNextBigM
 import ClientSpotlights from "./Components/ClientSpotlights/ClientSpotlights";
 import AIGITCGEN from "./Components/AIGITCGEN/AIGITCGEN";
 import Footer from "./Components/Global/Footer";
+import PageLoading from "./Components/Global/PageLoading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // window.onload = () => {
+    // };
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="bg-pri-dark w-full">
-      <Navbar />
-      <HeroSection />
-      <CodePG />
-      <TechStack />
-      <AboutSection />
-      <Leadership />
-      <MyNextBigMovesSection />
-      <ClientSpotlights />
-      <AIGITCGEN />
-      <Footer />
-    </div>
+    <>
+      {loading ? (
+        <PageLoading />
+      ) : (
+        <div className="bg-pri-dark w-full">
+          <Navbar />
+          <HeroSection />
+          <CodePG />
+          <TechStack />
+          <AboutSection />
+          <Leadership />
+          <MyNextBigMovesSection />
+          <ClientSpotlights />
+          <AIGITCGEN />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/Components/Global/PageLoading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageLoading = () => {
+  return (
+    <div className="w-full h-screen bg-pri-dark flex items-center justify-center">
+      <div className="w-10 h-10 border-4 border-white border-t-transparent border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default PageLoading;
